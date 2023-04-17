fa5b8bc7a7f76b0cae43a27cef12cd1f4afc52c7
Adeel.Tahir <Adeel.Tahir@cioxhealth.com>
2023-04-17 17:16:04 -0400

    Add url endpoint yearly data and final snippet of the project.

--- main.py
+++ main.py
@@ -25,6 +25,23 @@ def about(station, date):
             'Temp': temperature}
 
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(year)].to_dict(orient="records")
+    return result
+
+
 # This will allow you to see error in the webpage.
 if __name__ == "__main__":
     app.run(debug=True)
